e2a24fec6bfa1e36cdcad5585f9d4ab326a49800
RongX <sharoneaecs@gmail.com>
2019-06-30 11:12:36 -0700

    Initial Commit

---
+++ ShoppingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingApp
+//
+//  Created by Rong Xiao on 6/30/19.
+//  Copyright © 2019 Rong Xiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
