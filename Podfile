1c65483ad7b6359de9b1c769baba549c9a13d5c5
RongX <sharoneaecs@gmail.com>
2019-07-06 21:59:05 -0700

    pod installed firebase and progressHUD

---
+++ Podfile
@@ -0,0 +1,22 @@
+platform :ios, '9.0'
+
+target 'ShoppingApp' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ShoppingApp
+
+  target 'ShoppingAppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'ShoppingAppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  pod 'Firebase/Core'
+  pod 'SVProgressHUD'
+
+end
--- ShoppingApp/Controller/LoginViewController.swift
+++ ShoppingApp/Controller/LoginViewController.swift
@@ -7,18 +7,23 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func loginTapped(_ sender: Any) {
-        let mainTabCtler = storyboard?.instantiateInitialViewController() as! MainTabController
+        //let mainTabCtler = storyboard?.instantiateInitialViewController() as! MainTabController
         
-        present(mainTabCtler, animated: true, completion: nil)
+        //present(mainTabCtler, animated: true, completion: nil)
     }
     
 }
